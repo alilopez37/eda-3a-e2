f974766b4c21ca66bdf04ced357b0d67fa89eef0
alilopez <ali.lopez37@gmail.com>
2024-05-31 09:55:58 -0600

    First commit

---
+++ src/models/Student.mjs
@@ -0,0 +1,14 @@
+class Student {
+    matricula
+    nombre
+    apellidoPaterno
+
+    constructor(matricula, nombre, apellidoPaterno){
+        this.matricula = matricula
+        this.nombre = nombre
+        this.apellidoPaterno = apellidoPaterno
+    }
+    
+}
+
+export default Student
